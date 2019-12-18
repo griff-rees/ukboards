e5e749f8d2a89be79fe7fa33126db8ebdc45f4be
Dr Griffith Rees <griff.rees@gmail.com>
2019-12-18 00:56:17 +0000

    Add test_mock_get_charity_by_name.

--- tests/test_charities.py
+++ tests/test_charities.py
@@ -10,74 +10,32 @@ import pytest
 from zeep.plugins import HistoryPlugin
 
 from uk_boards.charities import (check_registered_charity_number, get_client,
-                                 CharitiesAuthPlugin)
+                                 CharitiesAuthPlugin, CHARITY_COMMISSION_WSDL)
 
 
-TEST_WSDL_CODE = 'http://a-test-api-code.uk/apiTest.asmx?wsdl'
-PHOTOGRAPHERS_GALLERY_NAME = (
-    "THE PHOTOGRAPHERS' GALLERY LTD                    "
-    "                                                  "
-    "                                                  ")
-PHOTOGRAPHERS_GALLERY_NUMBER = 262548
-
-
-TEST_SOAP_ENV = """\
-<soapenv:Envelope
-    xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
-    xmlns:v1="http://schemas.conversesolutions.com/xsd/dmticta/v1">
-<soapenv:Header/>
-<soapenv:Body>
-<v1:GetVehicleLimitedInfo>
-<v1:vehicleNo>?</v1:vehicleNo>
-<v1:phoneNo>?</v1:phoneNo>
-</v1:GetVehicleLimitedInfo>
-</soapenv:Body>
-</soapenv:Envelope>"""
+CHARITY_COMMISSION_API = CHARITY_COMMISSION_WSDL.split('?')[0]
 
 TEST_PHOTOGRAPHERS_GALLERY_NAME = (
     "THE PHOTOGRAPHERS' GALLERY LTD                    "
     "                                                  "
     "                                                  ")
 
-TEST_EMAIL = 'an.email@test.org.uk'
+TEST_TAG_NS = "http://www.charitycommission.gov.uk/"
 
-TEST_PHONE_NUMBER = '02075555555'
+TEST_PHOTOGRAPHERS_CHARITY_NUMBER = 262548
 
-TEST_SOAP_QUERY = f"""\
-<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-  <soap:Body>
-    <GetCharitiesByNameResponse xmlns="http://www.charitycommission.gov.uk/">
-      <GetCharitiesByNameResult>
-        <CharityList>
-          <RegisteredCharityNumber>
-            262548
-          </RegisteredCharityNumber>
-          <SubsidiaryNumber>
-            0
-          </SubsidiaryNumber>
-          <CharityName>
-            {TEST_PHOTOGRAPHERS_GALLERY_NAME}
-          </CharityName>
-          <MainCharityName>
-            {TEST_PHOTOGRAPHERS_GALLERY_NAME}
-          </MainCharityName>
-          <RegistrationStatus>
-            Registered
-          </RegistrationStatus>
-          <PublicEmailAddress>
-            {TEST_EMAIL}
-          </PublicEmailAddress>
-          <MainPhoneNumber>
-            {TEST_PHONE_NUMBER}
-          </MainPhoneNumber>
-        </CharityList>
-      </GetCharitiesByNameResult>
-    </GetCharitiesByNameResponse>
-  </soap:Body>
-</soap:Envelope>"""
+TEST_API_KEY = "A-fake-test-key"
+
+
+@pytest.fixture
+def test_client(requests_mock, maxlen: int = 20):
+    history = HistoryPlugin(maxlen=maxlen)
+    auth = CharitiesAuthPlugin(api_key_value=TEST_API_KEY)
+    with open('tests/charities_api.wsdl', 'r') as charities_api:
+        requests_mock.get(CHARITY_COMMISSION_WSDL, text=charities_api.read())
+        client = get_client(plugins=[history, auth])
+        assert hasattr(client.service, "GetCharitiesByName")
+        return client
 
 
 class TestZeepCharityClient:
@@ -86,15 +44,84 @@ class TestZeepCharityClient:
 
     def test_specify_config(self, requests_mock):
         """Basic test of generating correct Client configuration."""
+        TEST_WSDL_CODE = 'http://a-test-api-code.uk/apiTest.asmx?wsdl'
+
+        TEST_SOAP_ENV = """\
+        <soapenv:Envelope
+            xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+            xmlns:v1="http://schemas.conversesolutions.com/xsd/dmticta/v1">
+        <soapenv:Header/>
+        <soapenv:Body>
+        <v1:GetVehicleLimitedInfo>
+        <v1:vehicleNo>?</v1:vehicleNo>
+        <v1:phoneNo>?</v1:phoneNo>
+        </v1:GetVehicleLimitedInfo>
+        </soapenv:Body>
+        </soapenv:Envelope>"""
+
         requests_mock.get(TEST_WSDL_CODE, text=TEST_SOAP_ENV)
         test_client = get_client(TEST_WSDL_CODE)
         assert test_client.wsdl.location == TEST_WSDL_CODE
         assert test_client.settings.strict is False
         assert test_client.settings.xml_huge_tree is True
 
-    def test_mock_get_charity_by_name(self, requests_mock):
+    def test_mock_get_charity_by_name(self, requests_mock, test_client):
         """Test mock query of GetCharitiesByName."""
-        pass
+
+        TEST_EMAIL = 'an.email@test.org.uk'
+
+        TEST_PHONE_NUMBER = '02075555555'
+
+        TEST_SOAP_RESPONSE = f"""\
+        <?xml version="1.0" encoding="utf-8"?>
+        <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+          <soap:Body>
+            <GetCharitiesByNameResponse xmlns="{TEST_TAG_NS}">
+              <GetCharitiesByNameResult>
+                <CharityList>
+                  <RegisteredCharityNumber>
+                    {TEST_PHOTOGRAPHERS_CHARITY_NUMBER}
+                  </RegisteredCharityNumber>
+                  <SubsidiaryNumber>
+                    0
+                  </SubsidiaryNumber>
+                  <CharityName>
+                    {TEST_PHOTOGRAPHERS_GALLERY_NAME}
+                  </CharityName>
+                  <MainCharityName>
+                    {TEST_PHOTOGRAPHERS_GALLERY_NAME}
+                  </MainCharityName>
+                  <RegistrationStatus>
+                    Registered
+                  </RegistrationStatus>
+                  <PublicEmailAddress>
+                    {TEST_EMAIL}
+                  </PublicEmailAddress>
+                  <MainPhoneNumber>
+                    {TEST_PHONE_NUMBER}
+                  </MainPhoneNumber>
+                </CharityList>
+              </GetCharitiesByNameResult>
+            </GetCharitiesByNameResponse>
+          </soap:Body>
+        </soap:Envelope>"""
+
+        requests_mock.post(CHARITY_COMMISSION_API, text=TEST_SOAP_RESPONSE)
+        response = test_client.service.GetCharitiesByName(
+            strSearch=TEST_PHOTOGRAPHERS_GALLERY_NAME)
+        assert response[0]['CharityName'].strip() == (
+            TEST_PHOTOGRAPHERS_GALLERY_NAME.rstrip())
+        assert response[0]['RegisteredCharityNumber'] == (
+            TEST_PHOTOGRAPHERS_CHARITY_NUMBER)
+        assert response[0]['PublicEmailAddress'].strip() == TEST_EMAIL
+        assert response[0]['MainPhoneNumber'].strip() == TEST_PHONE_NUMBER
+
+        history = test_client.plugins[0]
+        assert history.last_received['envelope'][0][0].tag.endswith(
+            'GetCharitiesByNameResponse')
+        assert len(history._buffer) == 1
 
     @pytest.mark.remote_data
     def test_get_charity_by_name(self):
@@ -138,11 +165,11 @@ class TestGetCharityNumber:
         #                 'Showcharity/API/SearchCharitiesV1/'
         #                 'SearchCharitiesV1.asmx?wsdl')
         # history = HistoryPlugin()
-        # requests_mock.get(test_address, text=TEST_SOAP_QUERY)
+        # requests_mock.get(test_address, text=TEST_SOAP_RESPONSE)
         # history_client = get_client()
-        charity = check_registered_charity_number(PHOTOGRAPHERS_GALLERY_NAME,
-                                                  PHOTOGRAPHERS_GALLERY_NUMBER)
+        charity = check_registered_charity_number(
+            TEST_PHOTOGRAPHERS_GALLERY_NAME, TEST_PHOTOGRAPHERS_CHARITY_NUMBER)
         # charity = check_registered_charity_number(correct_charity_name,
         #                                           correct_charity_number,
         #                                           client=history_client)
-        assert charity == PHOTOGRAPHERS_GALLERY_NUMBER
+        assert charity == TEST_PHOTOGRAPHERS_CHARITY_NUMBER
