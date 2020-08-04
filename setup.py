c93d66892b6b622fb943a9ab47ab809286c9229a
Dr Griffith Rees <griff.rees@gmail.com>
2020-08-04 16:48:33 +0100

    Remove python 3.6 support.

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: Information Analysis',
