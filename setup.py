4b99b2cfbae97181c07b0c42eb98fe7ce7e0a4ce
Dr Griffith Rees <griff.rees@gmail.com>
2021-09-04 14:29:53 +0100

    Bump version: 0.5.3 → 0.5.4

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     tests_require=test_requirements,
     url="https://github.com/griff-rees/ukboards",
     # fmt: off
-    version='0.5.3',
+    version='0.5.4',
     # fmt: on
     zip_safe=False,
 )
